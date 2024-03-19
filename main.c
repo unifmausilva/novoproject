b28f108b19afe89a3e9f1ae8ae875aa1cc376ada
unifmausilva <161088037+unifmausilva@users.noreply.github.com>
2024-03-19 23:21:13 +0000

    novo

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+  int opção;
+  do {
+
+    printf("\nMenu princiapl\n");
+    printf("1 - Criar tarefa \n");
+    printf("2 - Deletar tarefa \n");
+    printf("3 - Listar tarefas \n");
+    printf("0 - Sair \n");
+    printf("Entre com uma opção:  \n");
+    int i = scanf("%d", &opção);
+    printf("Opção escolhida: %d\n", opção);
+  } while (opção != 0);
 }
